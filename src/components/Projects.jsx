ab3ba49cd09ddc6ce25aa4ce72a8f59e4fb83a2a
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-08-13 16:59:43 -0700

    Only Tailwind

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,12 +2,13 @@ import * as React from "react"
 
 function Projects() {
     return (
-        <div className="bg-cover h-[50vh] flex justify-center items-center text-center px-[300px] py-[50px] bg-[url(public/stock-satellite-image.jpeg)]">
+        <div className="bg-cover h-[50vh] font-[Gilroy-Bold] flex justify-center items-center text-center px-[300px] py-[50px] bg-[url(public/stock-satellite-image.jpeg)]">
             <div>
-                <h1 style={{fontFamily: "Gilroy-Bold", fontSize: "70px", color: "#E5A823"}}>
+             
+                <h1 className="text-[70px] text-[#E5A823]"> 
                     projects
                 </h1>
-                <p style={{fontFamily: "Gilroy-Bold", color: "white", fontSize: "20px"}}>
+                <p className="text-[20px] text-[white]">
                     Explore a realm of limitless possibilities with SJSU Cube3 projects.Venture into the universe of miniature satellites 
                     as we develop a CanSat alongside new members. Witness the magic as this compact marvel undertakes a myriad of tasks during 
                     its descent from great heights – from deploying a heatshield to capturing video footage and landing gracefully, all 
