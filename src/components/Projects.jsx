540c8d66c9ee04aca7c5f36cc708af1d0ffc8008
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-08-17 17:53:17 -0700

    Add images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,12 +6,11 @@ function Projects() {
     return (
         <Section className={"!h-[50vh] font-[Gilroy-Bold] text-center px-[300px] py-[50px]"}  style={{ background: `url(${IMAGES.projectBackground}) no-repeat center`, backgroundSize: "cover" }}>
             <div>
-             
                 <h1 className="text-[70px] text-[var(--club-yellow)]"> 
                     projects
                 </h1>
                 <p className="text-[20px] text-[white]">
-                    Explore a realm of limitless possibilities with SJSU Cube3 projects.Venture into the universe of miniature satellites 
+                    Explore a realm of limitless possibilities with SJSU Cube3 projects. Venture into the universe of miniature satellites 
                     as we develop a CanSat alongside new members. Witness the magic as this compact marvel undertakes a myriad of tasks during 
                     its descent from great heights – from deploying a heatshield to capturing video footage and landing gracefully, all 
                     within the dimensions of a soda can. Alternatively, immerse yourself in the Nodes Project, crafting a sensor network 
